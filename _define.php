b48a7071b84bd40d9ae4d3461194934d225cf3f0
brol <brol31@gmail.com>
2015-05-05 18:43:50 +0200

    1.2

---
+++ _define.php
@@ -0,0 +1,22 @@
+<?php
+# -- BEGIN LICENSE BLOCK ----------------------------------
+#
+# This file is part of Graylvetica, a Dotclear 2 theme.
+#
+# Copyright (c) 2003-2015 Olivier Meunier and contributors
+# Licensed under the GPL version 2.0 license.
+# See LICENSE file or
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+#
+# -- END LICENSE BLOCK ------------------------------------
+if (!defined('DC_RC_PATH')) { return; }
+
+$this->registerModule(
+	/* Name */			"Graylvetica",
+	/* Description*/		"Helvetica on red, black and gray",
+	/* Author */			"Olivier Meunier, Pierre Van Glabeke",
+	/* Version */			'1.2',
+	array(
+		'type'		=>		'theme'
+	)
+);
